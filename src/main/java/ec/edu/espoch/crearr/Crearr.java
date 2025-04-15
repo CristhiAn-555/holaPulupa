3ee072a1ad5e0720c5777ade4dbfecb5e9bdebe5
CristhiAn-555 <cristhianvalencia999@gmail.com>
2025-04-15 09:16:51 -0500

    holaa

---
+++ src/main/java/ec/edu/espoch/crearr/Crearr.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package ec.edu.espoch.crearr;
+
+/**
+ *
+ * @author PERSONAL
+ */
+public class Crearr {
+
+    public static void main(String[] args) {
+        System.out.println("Hola pulupa, como estas");
+    }
+}
